9d0f7c3d6b762dc289dfbecc685e48f3ebcf6dbd
Luckysting <work.romanuel@gmail.com>
2020-05-02 20:29:09 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Pharmacie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Pharmacy = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Medicine = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Quantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Expires = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SearchField = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Pharmacy,
+            this.Medicine,
+            this.Quantity,
+            this.Price,
+            this.Date,
+            this.Expires});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 67);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1075, 371);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Pharmacy
+            // 
+            this.Pharmacy.HeaderText = "Аптека";
+            this.Pharmacy.MinimumWidth = 6;
+            this.Pharmacy.Name = "Pharmacy";
+            this.Pharmacy.Width = 125;
+            // 
+            // Medicine
+            // 
+            this.Medicine.HeaderText = "Лекарство";
+            this.Medicine.MinimumWidth = 6;
+            this.Medicine.Name = "Medicine";
+            this.Medicine.Width = 125;
+            // 
+            // Quantity
+            // 
+            this.Quantity.HeaderText = "В наличии";
+            this.Quantity.MinimumWidth = 6;
+            this.Quantity.Name = "Quantity";
+            this.Quantity.Width = 125;
+            // 
+            // Price
+            // 
+            this.Price.HeaderText = "Цена за упаковку";
+            this.Price.MinimumWidth = 6;
+            this.Price.Name = "Price";
+            this.Price.Width = 125;
+            // 
+            // Date
+            // 
+            this.Date.HeaderText = "Дата поставки";
+            this.Date.MinimumWidth = 6;
+            this.Date.Name = "Date";
+            this.Date.Width = 125;
+            // 
+            // Expires
+            // 
+            this.Expires.HeaderText = "Испортится через";
+            this.Expires.MinimumWidth = 6;
+            this.Expires.Name = "Expires";
+            this.Expires.Width = 125;
+            // 
+            // SearchField
+            // 
+            this.SearchField.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SearchField.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SearchField.Location = new System.Drawing.Point(12, 12);
+            this.SearchField.Name = "SearchField";
+            this.SearchField.Size = new System.Drawing.Size(802, 34);
+            this.SearchField.TabIndex = 1;
+            this.SearchField.TextChanged += new System.EventHandler(this.SearchField_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(983, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Reload";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(873, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 33);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Commit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1099, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SearchField);
+            this.Controls.Add(this.dataGridView1);
+            this.MinimumSize = new System.Drawing.Size(1117, 497);
+            this.Name = "Form1";
+            this.Text = "Аптечная база";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox SearchField;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Pharmacy;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Medicine;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Quantity;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Date;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Expires;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Pharmacie
+{
+    public partial class Form1 : Form
+    {
+        private IEnumerable<object[]> stored;
+        public Form1()
+        {
+            InitializeComponent();
+            fill();
+        }
+
+        private void fill()
+        {
+            var filename = "db.xml";
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var purchaseOrderFilepath = Path.Combine(currentDirectory, filename);
+            var doc = XElement.Load(purchaseOrderFilepath);
+            var elemets = from item in doc.Elements("Item")
+                          select new object[] {
+                            item.Element("Pharmacy").Value,
+                            item.Element("Medicine").Value,
+                            item.Element("Quantity").Value,
+                            item.Element("Price").Value,
+                            item.Element("Date").Value,
+                            item.Element("Expires").Value
+                          };
+            stored = elemets;
+            this.dataGridView1.Rows.Clear();
+            foreach (var element in elemets)
+            {
+                this.dataGridView1.Rows.Add(element);
+            }
+        }
+
+        private void commit()
+        {
+            var filename = "db.xml";
+            var doc = new XElement("Items");
+            foreach(DataGridViewRow row in this.dataGridView1.Rows)
+            {
+                try
+                {
+                    XElement item = new XElement("Item",
+                    new XElement("Pharmacy", row.Cells[0].Value.ToString()),
+                    new XElement("Medicine", row.Cells[1].Value.ToString()),
+                    new XElement("Quantity", row.Cells[2].Value.ToString()),
+                    new XElement("Price", row.Cells[3].Value.ToString()),
+                    new XElement("Date", row.Cells[4].Value.ToString()),
+                    new XElement("Expires", row.Cells[5].Value.ToString())
+                    );
+                    doc.Add(item);
+                } catch (NullReferenceException exp)
+                {
+                    if (row.Index != this.dataGridView1.Rows.Count - 1)
+                    {
+                        MessageBox.Show("Row " + row.Index + " has wrong values");
+                    }
+                }
+            }
+            doc.Save(filename);
+            fill();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.SearchField.Text = "";
+            fill();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (this.SearchField.Text == "")
+            {
+                commit();
+            } else
+            {
+                MessageBox.Show("Commit blocked while search input is not empty");
+            }
+        }
+
+        private void SearchField_TextChanged(object sender, EventArgs e)
+        {
+            var txt = this.SearchField.Text.ToLower();
+            if (txt != "")
+            {
+                var elemets = from item in this.stored
+                              where item[0].ToString().ToLower().Contains(txt) || item[1].ToString().ToLower().Contains(txt) || item[4].ToString().ToLower().Contains(txt)
+                              select item;
+                this.dataGridView1.Rows.Clear();
+                foreach (var element in elemets)
+                {
+                    this.dataGridView1.Rows.Add(element);
+                }
+            } else
+            {
+                this.dataGridView1.Rows.Clear();
+                foreach (var element in this.stored)
+                {
+                    this.dataGridView1.Rows.Add(element);
+                }
+            }
+        }
+    }
+}
